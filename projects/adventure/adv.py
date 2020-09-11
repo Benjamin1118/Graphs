cc8602b55f9f74da8d41dccf01d19216d2ce01cb
Benjamin Bishop <benjaminbishop12@gmail.com>
2020-09-11 11:40:09 -0600

    working on secondary solution

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,7 +10,7 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file =  "projects\adventure\maps\test_line.txt"  #"maps/test_line.txt" 
+# map_file =  "maps/test_line.txt" 
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt" 
@@ -31,35 +31,54 @@ traversal_path = []
 
 # Start tansverse test code
 opposite = {'n': 's', 's': 'n', 'e':'w', 'w': 'e'}
-
-
-def traverse(room, visited = None):
-    if visited is None:
-        visited = set()
-
-    # create a path list
-    path = []
+def travel(visited = set(), path):
+    #set up place to get closest room with open direction (where we haven't explored yet)
+    nearest = []
+    #starts in this room
     room = player.current_room
-
-    # Go through possible directions
     for direction in room.get_exits():
         player.travel(direction)
-        room = player.current_room
-
-        # if room is visited
         if room in visited:
             player.travel(opposite[direction])
-            # else add room to visited and direction to path
         else:
-            visited.add(room)
-            path.append(direction)
-            #recursivly call function again on this room and add to path
-            path += traverse(room, visited)
-            player.travel(opposite[direction])
-            path.append(opposite[direction])
-    return path
+            room = player.current_room
+
+
+
+
+
+
+# soltion round 1
+# opposite = {'n': 's', 's': 'n', 'e':'w', 'w': 'e'}
+
+
+# def traverse(room, visited = None):
+#     if visited is None:
+#         visited = set()
+
+#     # create a path list
+#     path = []
+#     room = player.current_room
+
+#     # Go through possible directions
+#     for direction in room.get_exits():
+#         player.travel(direction)
+#         room = player.current_room
+
+#         # if room is visited
+#         if room in visited:
+#             player.travel(opposite[direction])
+#             # else add room to visited and direction to path
+#         else:
+#             visited.add(room)
+#             path.append(direction)
+#             #recursivly call function again on this room and add to path
+#             path += traverse(room, visited)
+#             player.travel(opposite[direction])
+#             path.append(opposite[direction])
+#     return path
                 
-traversal_path = traverse(player.current_room)
+# traversal_path = traverse(player.current_room)
 
 # TRAVERSAL TEST
 visited_rooms = set()
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -14,7 +14,7 @@ class Graph:
         Add a vertex to the graph.
         """
         self.vertices[vertex_id]= set()
-        pass  # TODO
+        
 
     def add_edge(self, v1, v2):
         """
@@ -24,14 +24,13 @@ class Graph:
             self.vertices[v1].add(v2)
         else:
             raise IndexError("vertex does not Exist")
-        pass  # TODO
+        
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
         return self.vertices[vertex_id]
-        pass  # TODO
 
     def bft(self, starting_vertex):
         """
@@ -55,10 +54,11 @@ class Graph:
             # if vertex has not been visited
             if v not in visited:
                 # mark vertex as visited
-                visited.add(v)
                 # print it for debug
                 print(v)
 
+                visited.add(v)
+                
                 # add all of it's neighbors to the back of the queue
                 for next_vertex in self.get_neighbors(v):
                     q.enqueue(next_vertex)
@@ -94,20 +94,20 @@ class Graph:
                     s.push(next_vertex)
         pass  # TODO
 
-    def dft_recursive(self, starting_vertex, visited = None):
+    def dft_recursive(self, starting_vertex, visited = set()):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        if visited is None:
-            visited = set()
         visited.add(starting_vertex)
         print(starting_vertex)
 
         for v in self.get_neighbors(starting_vertex):
-            if v not in visited:
+            if v in visited:
+                return
+            else:
                 self.dft_recursive(v, visited)
         pass  # TODO
 
@@ -193,7 +193,7 @@ class Graph:
         return None
         pass  # TODO
 
-    def dfs_recursive(self, starting_vertex,  visited=None):#destination_vertex):
+    def dfs_recursive(self, starting_vertex, target_vertex_id, visited= set(), path = []):#destination_vertex):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -201,18 +201,21 @@ class Graph:
 
         This should be done using recursion.
         """
-        if visited is None:
-            visited = set()
-        #visited.add(starting_vertex)
-        visited.add(starting_vertex)
-        print(starting_vertex)
+        temp_path = path.copy()
+        temp_path.append(starting_vertex)
 
-        for v in self.get_neighbors(starting_vertex):
-            if v not in visited:
-                self.dft_recursive(v, visited)
+        if starting_vertex not in visited:
+            # print(starting_vertex)
+            visited.add(starting_vertex)
+
+            if starting_vertex == target_vertex_id:
+                return temp_path
+            else:
+                for neighbor in self.get_neighbors(starting_vertex):
+                    next_path = self.dfs_recursive(neighbor, target_vertex_id, visited, temp_path)
+                    if next_path is not None:
+                        return next_path
         
-        
-        pass  # TODO
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
